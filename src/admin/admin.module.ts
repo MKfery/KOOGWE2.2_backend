67ed2b286a38efe40c2cf785aa8821d01c6b3112
MKfery <ferytail360@gmail.com>
2026-04-06 23:27:17 +0000

    commit

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -4,9 +4,17 @@ import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 import { AppGateway } from '../common/websocket.gateway';
 import { MailModule } from '../mail/mail.module';
+import { PrismaModule } from '../prisma/prisma.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [MailModule],
+  imports: [
+    MailModule,
+    PrismaModule,
+    JwtModule.register({
+      secret: process.env.JWT_ACCESS_SECRET,
+    }),
+  ],
   controllers: [AdminController],
   providers: [AdminService, AppGateway],
   exports: [AdminService],
