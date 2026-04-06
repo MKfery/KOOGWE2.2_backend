de9b0fd6e66575b85af5c8fb46f285a83ac29826
MKfery <ferytail360@gmail.com>
2026-04-06 23:22:17 +0000

    commit

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -2,10 +2,13 @@
 import { Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
+import { AppGateway } from '../common/websocket.gateway';
+import { MailModule } from '../mail/mail.module';
 
 @Module({
+  imports: [MailModule],
   controllers: [AdminController],
-  providers: [AdminService],
+  providers: [AdminService, AppGateway],
   exports: [AdminService],
 })
 export class AdminModule {}
